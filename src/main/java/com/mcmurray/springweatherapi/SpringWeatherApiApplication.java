8b14630d77b92eb0e9e1bef0dfb70685a43c51b1
Mary McMurray <marymcmurray99@gmail.com>
2022-02-06 18:37:15 -0500

    init

---
+++ src/main/java/com/mcmurray/springweatherapi/SpringWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.mcmurray.springweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeatherApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringWeatherApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mcmurray/springweatherapi/SpringWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcmurray.springweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeatherApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
