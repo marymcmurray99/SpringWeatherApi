e95e642866a6bcf3da86057126a904403ae5114d
Mary McMurray <marymcmurray99@gmail.com>
2022-02-07 12:35:22 -0500

    Finish Properties
    - Add metadata
    - add java class config

--- src/main/java/com/mcmurray/springweatherapi/CustomProperties.java
+++ src/main/java/com/mcmurray/springweatherapi/CustomProperties.java
@@ -1,4 +1,11 @@
 package com.mcmurray.springweatherapi;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "com.mcmurray.springweatherapi")
 public class CustomProperties {
+  String openWeatherMapApiKey;
+  String apiCurrentWeatherData;
 }
